063770276c3e9ffff9d40c4b8680c76476cb29ff
Lukas Herman <lherman.cs@gmail.com>
2018-10-12 20:29:43 -0400

    Finished up

--- sde-2/helpers.ml
+++
@@ -1,8 +0,0 @@
-let rec minElement = function 
-  [] -> failwith "error with empty list"
-  | [last] -> last
-  | h::t -> min h (minElement(t))
-
-let rec find = function
-  ([], x) -> -1
-  | (h::t, x) -> if h = x then 0 else 1 + find(t, x)
